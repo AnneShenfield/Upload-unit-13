d7cc22d7c18ff61d9a954c23902605844d2ff097
Annalee Shenfield <anneshenfield@gmail.com>
2023-03-31 20:19:32 -0400

    This change will show the Unit 10 assignment.py.

---
+++ anna Unit 10 assignment.py
@@ -0,0 +1,16 @@
+class Table:
+  def __init__(self, color, legs):
+    self. color = color
+    self.legs = legs
+
+Table1 = Table("Brown Table", 4)
+Table2 = Table ("White Table", 3)
+
+print(Table1.color)
+print(Table1.legs)
+print(Table2.color)
+print(Table2.legs)
+
+
+print ("The total legs in the room for both tables are 7")
+print ("The white table has 1 extra leg than the brown table")
